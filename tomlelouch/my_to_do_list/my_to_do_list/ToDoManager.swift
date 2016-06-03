793234439923ada70d64b89b050259f1711a0289
tomlelouch <tom.tang@vpon.com>
2016-06-03 23:01:22 +0800

    init the toDoList app

---
+++ tomlelouch/my_to_do_list/my_to_do_list/ToDoManager.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoManager.swift
+//  my_to_do_list
+//
+//  Created by tomlelouch on 6/3/16.
+//  Copyright © 2016 tomlelouch. All rights reserved.
+//
+
+import UIKit
+
+var toDoMgr: ToDoManager = ToDoManager()
+
+struct toDo {
+    var title = "no-title"
+    var desc  = "no-desc"
+}
+
+class ToDoManager: NSObject {
+
+}
